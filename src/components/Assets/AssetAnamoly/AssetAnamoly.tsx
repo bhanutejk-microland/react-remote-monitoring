689a6a0c26158b3b11bf989d20e1fa815c39f275
bhanutejk-microland <bhanutejk@microland.com>
2020-05-28 15:52:46 +0530

    added skeleton

--- src/components/Assets/AssetAnamoly/AssetAnamoly.tsx
+++ src/components/Assets/AssetAnamoly/AssetAnamoly.tsx
@@ -5,6 +5,7 @@ import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import Typography from "@material-ui/core/Typography";
+import Skeleton from '@material-ui/lab/Skeleton';
 
 import AnomalyBulletChart from '../../Charts/AnomalyBulletChart';
 
@@ -16,7 +17,38 @@ interface AssetAnamolyProps {
 const AssetAnamoly: FunctionComponent<AssetAnamolyProps> = ({ anamoly, indexing }) => {
 
   const renderAssetAnamolyContent = () => {
-    return Object.keys(anamoly).map((anomalyKey, indexing) => {
+    if (Object.keys(anamoly).length > 0) {
+      return Object.keys(anamoly).map((anomalyKey, indexing) => {
+        return (
+          <Grid container spacing={2} key={indexing}>
+            <Grid item xs={12}>
+              <ExpansionPanel defaultExpanded>
+                <ExpansionPanelSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1c-content"
+                  id="panel1c-header"
+                // className={classes.AssetPanelHeader}
+                >
+                  <div className="">
+                    <Typography className="">
+                      <b style={{ textTransform: 'uppercase' }}>{anomalyKey}</b>
+                    </Typography>
+                  </div>
+
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails className="">
+                  <AnomalyBulletChart
+                    indexing={indexing}
+                    data={[...anamoly[anomalyKey]]}
+                    chartHeight="300px"
+                  />
+                </ExpansionPanelDetails>
+              </ExpansionPanel>
+            </Grid>
+          </Grid>
+        );
+      });
+    } else {
       return (
         <Grid container spacing={2} key={indexing}>
           <Grid item xs={12}>
@@ -29,23 +61,19 @@ const AssetAnamoly: FunctionComponent<AssetAnamolyProps> = ({ anamoly, indexing 
               >
                 <div className="">
                   <Typography className="">
-                    <b style={{ textTransform: 'uppercase' }}>{anomalyKey}</b>
+                    <b style={{ textTransform: 'uppercase' }}><Skeleton variant="text" width={100} /></b>
                   </Typography>
                 </div>
 
               </ExpansionPanelSummary>
               <ExpansionPanelDetails className="">
-                <AnomalyBulletChart
-                  indexing={indexing}
-                  data={[...anamoly[anomalyKey]]}
-                  chartHeight="300px"
-                />
+                <Skeleton variant="rect" width="100%" height={118} />
               </ExpansionPanelDetails>
             </ExpansionPanel>
           </Grid>
         </Grid>
       );
-    });
+    }
   }
 
   return (
