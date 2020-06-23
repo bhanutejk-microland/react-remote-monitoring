55caff787ef41d838846cdbd3668b5b7d128e6a7
shashankag-microland <ShashankAg@microland.com>
2020-06-23 15:19:01 +0530

    Added new menu as AR Assit

--- src/components/LeftMenu/LeftMenu.tsx
+++ src/components/LeftMenu/LeftMenu.tsx
@@ -3,6 +3,10 @@ import React, { FunctionComponent } from "react";
 import classes from "./LeftMenu.css";
 import MenuItem from "./MenuItem/MenuItem";
 
+//temporary Added
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 interface LeftMenu {
   label: string;
   path: string;
@@ -25,16 +29,35 @@ const LeftMenu: FunctionComponent<LeftMenuProps> = ({
         : classes.MainSidebar
     }
   >
-    {menuList.map((item: LeftMenu) => {
-      return (
-        <MenuItem
-          key={item.label}
-          label={item.label}
-          path={item.path}
-          menuIcon={item.menuIcon}
-          showMiniLeftMenu={showMiniLeftMenu}
-        />
-      );
+    {menuList.map((item: LeftMenu) => {      
+      if(item.label === "AR Assist"){
+        return(
+          <Link key={item.label}
+                style={{color: "#ffffff", display: "flex", alignItems: "center", cursor: "pointer", textDecoration: "none", borderLeft: "3px solid transparent"}}
+                className={classes.MenuItemWrapper}
+                target="_blank"
+                to={item.path}
+                onClick={(event) => {event.preventDefault(); window.open("https://connect.vuforia.com/");}}>
+            <FontAwesomeIcon icon={item.menuIcon} style={{padding: "17px"}} />
+            <span
+              className={showMiniLeftMenu ? classes.MiniMenuLabel : classes.MenuLabel}
+            >
+              {item.label}
+            </span>
+          </Link>
+        )
+      }else{
+        return (
+          <MenuItem
+            key={item.label}
+            label={item.label}
+            path={item.path}
+            menuIcon={item.menuIcon}
+            showMiniLeftMenu={showMiniLeftMenu}
+          />
+        );
+      }
+      
     })}
   </aside>
 );
